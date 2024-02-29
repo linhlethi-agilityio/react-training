fc6dbe1430c0ece5c1800d1ffbcdde33ea60cb1a
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-02-29 09:28:56 +0700

    Update alignItems for content PaymentDetailModal

--- crud-operations/src/components/Modal/PaymentDetailModal/index.tsx
+++ crud-operations/src/components/Modal/PaymentDetailModal/index.tsx
@@ -25,27 +25,27 @@ const PaymentDetailModal = ({
   return (
     <CustomModal isOpen={isOpen} onClose={onClose} title="Payment Detail">
       <Box paddingX="40px" paddingBottom="40px">
-        <Flex justifyContent="space-between">
+        <Flex justifyContent="space-between" alignItems="center">
           <Heading size="small">Name:</Heading>
           <Text size="small">{name}</Text>
         </Flex>
-        <Flex justifyContent="space-between" marginTop="10px">
+        <Flex justifyContent="space-between" marginTop="10px" alignItems="center">
           <Heading size="small">Payment Schedule:</Heading>
           <Text size="small">{paymentSchedule}</Text>
         </Flex>
-        <Flex justifyContent="space-between" marginTop="10px">
+        <Flex justifyContent="space-between" marginTop="10px" alignItems="center">
           <Heading size="small">Bill Number:</Heading>
           <Text size="small">INR {billNumber}</Text>
         </Flex>
-        <Flex justifyContent="space-between" marginTop="10px">
+        <Flex justifyContent="space-between" marginTop="10px" alignItems="center">
           <Heading size="small">Amount Paid:</Heading>
           <Text size="small">INR {amountPaid}</Text>
         </Flex>
-        <Flex justifyContent="space-between" marginTop="10px">
+        <Flex justifyContent="space-between" marginTop="10px" alignItems="center">
           <Heading size="small">Balance amount:</Heading>
           <Text size="small">INR {balanceAmount}</Text>
         </Flex>
-        <Flex justifyContent="space-between" marginTop="10px">
+        <Flex justifyContent="space-between" marginTop="10px" alignItems="center">
           <Heading size="small">Date:</Heading>
           <Text size="small">{date}</Text>
         </Flex>
